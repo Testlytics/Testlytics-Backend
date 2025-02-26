bf18bb9e0e55fb960e710e19e1870fd9835a0fb8
Adith7923 <adith7923@gmail.com>
2025-02-26 12:44:09 +0530

    initial commit

---
+++ src/main/java/com/example/Testlytics/TestlyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Testlytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestlyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestlyticsApplication.class, args);
+	}
+
+}
